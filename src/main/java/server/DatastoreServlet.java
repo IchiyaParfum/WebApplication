fbe7d498b1784dd657cd0b9ac3ead2d400c01a55
mstieger <mstieger@192.168.1.46>
2018-12-25 20:06:34 +0100

    Enabled file export for Json

--- src/main/java/server/DatastoreServlet.java
+++ src/main/java/server/DatastoreServlet.java
@@ -9,11 +9,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 
 @WebServlet(
     name = "DatastoreServlet",
-    urlPatterns = {"/Datastore"}
+    urlPatterns = {"/Datastore/*"}
 )
 public class DatastoreServlet extends HttpServlet {
 	private DatastoreManager ds;
@@ -23,10 +25,35 @@ public class DatastoreServlet extends HttpServlet {
 	}
 	
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) 
-      throws IOException {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	 //setAccessControlHeaders(response);
+	String responseString = getResponseContent(request, response);
+	setResponseContent(response, responseString, HttpServletResponse.SC_OK);
   }
   
+  private String getResponseContent(HttpServletRequest request, HttpServletResponse response){
+	  	String requestedRes = request.getRequestURI().substring(request.getRequestURI().lastIndexOf("/") + 1);
+		response.setHeader("Content-disposition","attachment; filename="+ requestedRes);
+		
+		if(requestedRes.endsWith(".csv")) {
+			return new String();
+		}else if(requestedRes.endsWith(".json")){
+			return doGetAsJson(request, response);
+		}
+		return new String();
+	}
+  
+  	private String doGetAsJson(HttpServletRequest request, HttpServletResponse response){
+		response.setContentType ("application/json");
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		return gson.toJson(ds.queryGSon("myFirstSensor"));
+  	}
+	
+	private void setResponseContent(HttpServletResponse response, String content, int status) throws IOException {
+		response.setStatus(status);
+		response.getWriter().write(content);
+	}
+		
   @Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	  InputStreamReader is = new InputStreamReader(req.getInputStream());
