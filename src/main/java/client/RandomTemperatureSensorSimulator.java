7cc51e65fda2d7393338342fda1c11b597f42c8b
mstieger <mstieger@192.168.1.46>
2018-12-25 16:58:59 +0100

    Connect to server before starting thread => Client didnt have connection during running while there was no problem in debugging mode

--- src/main/java/client/RandomTemperatureSensorSimulator.java
+++ src/main/java/client/RandomTemperatureSensorSimulator.java
@@ -6,9 +6,11 @@ public class RandomTemperatureSensorSimulator {
 
 	public static void main(String[] args) {
 		try {
-			TemperatureSensor ts = new TemperatureSensor("myFirstSensor", "http://localhost:8080/Datastore");
-			new Thread(ts).start();
+			TemperatureSensor ts = new TemperatureSensor("myFirstSensor", "http://webapplication-226612.appspot.com/Datastore");
+			//TemperatureSensor ts = new TemperatureSensor("myFirstSensor", "http://localhost:8080/Datastore");
 			ts.connect();
+			new Thread(ts).start();
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
